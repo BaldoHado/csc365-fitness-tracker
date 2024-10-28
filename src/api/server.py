2fe4ba70a63fb38ca162e7f4df3558272cd6d96b
Adam Ta <adamta2004@gmail.com>
2024-10-28 10:45:10 -0700

    FastAPI + root endpoint setup

---
+++ src/api/server.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI, exceptions
+from fastapi.responses import JSONResponse
+from pydantic import ValidationError
+from src.api import backend
+import json
+import logging
+import sys
+from starlette.middleware.cors import CORSMiddleware
+
+description = "Fitness Tracker App for CSC 365"
+
+app = FastAPI(
+    title="Fitness-Tracker",
+    description=description,
+    version="0.0.1",
+    terms_of_service="http://example.com/terms/",
+    contact={
+        "name": "Adam Ta",
+        "email": "ajta@calpoly.edu",
+    },
+)
+
+origins = ["https://example.com"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "OPTIONS"],
+    allow_headers=["*"],
+)
+
+app.include_router(backend.router)
+
+
+@app.exception_handler(exceptions.RequestValidationError)
+@app.exception_handler(ValidationError)
+async def validation_exception_handler(request, exc):
+    logging.error(f"The client sent invalid data!: {exc}")
+    exc_json = json.loads(exc.json())
+    response = {"message": [], "data": None}
+    for error in exc_json:
+        response["message"].append(f"{error['loc']}: {error['msg']}")
+
+    return JSONResponse(response, status_code=422)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Fitness Tracker Root Endpoint"}
