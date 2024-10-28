83dac3f8f7746b7a61092eb150a97760e6c44520
Kaila Aquino <kailad.aquino@gmail.com>
2024-10-28 13:48:17 -0700

    post user

--- src/api/users.py
+++ src/api/users.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends
 from src.api import auth
 from src.utils import data_utils
+import sqlalchemy
+from src import database as db
 
 router = APIRouter(
     prefix="/users",
@@ -8,6 +10,29 @@ router = APIRouter(
     dependencies=[Depends(auth.get_api_key)],
 )
 
-@router.post("/")
-def post_user():
-    return "OK"
+# adds new user to data base 
+@router.post("/{first_name}/{last_name}")
+def post_user(first_name, last_name):
+    # with db.engine.begin() as connection:
+    #     connection.execute(sqlalchemy.text("UPDATE users SET first_name"))
+
+    with db.engine.begin() as connection:
+            connection.execute(sqlalchemy.text("UPDATE users"
+                                                "SET first_name = :first_name, "
+                                                "last_name = :last_name"),
+                                                {
+                                                      "first_name": first_name,
+                                                      "last_name": last_name
+                                                })
+
+    return {
+        "first_name": first_name,
+        "last_name": last_name
+    }
+
+# @router.post("/")
+# def post_user():
+#     # with db.engine.begin() as connection:
+#     #     connection.execute(sqlalchemy.text("UPDATE users SET first_name"))
+       
+#     return "OK"
