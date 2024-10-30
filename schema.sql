5d8107f21a14aafd418939db27aef918f5b5c921
Adam Ta <adamta2004@gmail.com>
2024-10-30 13:37:36 -0700

    reformat users and update schema

--- schema.sql
+++ schema.sql
@@ -1,27 +1,17 @@
-CREATE TABLE muscle_groups (
-    muscle_id bigserial NOT NULL PRIMARY KEY,
-    muscle_name text NOT NULL
-);
-
 CREATE TABLE users (
     user_id bigserial NOT NULL PRIMARY KEY,
     first_name text NOT NULL,
     last_name text NOT NULL
 );
 
-CREATE TABLE workout_item (
-    workout_id bigserial NOT NULL PRIMARY KEY,
+CREATE TABLE user_workout_item (
+    user_id bigint REFERENCES users (user_id),
+    workout_id bigint REFERENCES workout (workout_id),
     sets int NOT NULL,
     reps int NOT NULL,
     weight int NOT NULL,
     rest_time int NOT NULL,
-    muscle_id bigint REFERENCES muscle_groups (muscle_id)
-);
-
-CREATE TABLE user_workout_item (
-    user_id bigint REFERENCES users (user_id),
-    workout_item_id bigint REFERENCES workout_item (workout_id),
-    PRIMARY KEY (user_id, workout_item_id)
+    PRIMARY KEY (user_id, workout_id) 
 );
 
 CREATE TABLE workout (
--- src/api/users.py
+++ src/api/users.py
@@ -10,26 +10,24 @@ router = APIRouter(
     dependencies=[Depends(auth.get_api_key)],
 )
 
-# adds new user to data base 
 @router.post("/{first_name}/{last_name}")
-def post_user(first_name, last_name):
+def post_user(first_name: str, last_name: str):
     with db.engine.begin() as connection:
-            connection.execute(sqlalchemy.text("INSERT INTO users (first_name, last_name) "
-                                                "VALUES (:first_name, :last_name)"),
-                                                {
-                                                      "first_name": first_name,
-                                                      "last_name": last_name
-                                                })
+        connection.execute(
+            sqlalchemy.text(
+                "INSERT INTO users (first_name, last_name) "
+                "VALUES (:first_name, :last_name)"
+            ),
+            {"first_name": first_name, "last_name": last_name},
+        )
 
-    return {
-        "first_name": first_name,
-        "last_name": last_name
-    }
+    return {"first_name": first_name, "last_name": last_name}
+
 
 # @router.post("/")
 # def post_workouts(user_list: list):
 
 #     # with db.engine.begin() as connection:
 #     #     connection.execute(sqlalchemy.text("UPDATE users SET first_name"))
-       
+
 #     return "OK"
