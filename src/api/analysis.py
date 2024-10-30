47e66a3cc49003ae4c7d98fbdd6c2e0cba9cb866
Adam Ta <adamta2004@gmail.com>
2024-10-30 10:49:29 -0700

    Add workout distribution

--- src/api/analysis.py
+++ src/api/analysis.py
@@ -5,6 +5,7 @@ import sqlalchemy
 from src import database as db
 from src.utils import analysis_utils, utils
 from typing import List
+from collections import Counter
 
 router = APIRouter(
     prefix="/analysis",
@@ -14,7 +15,8 @@ router = APIRouter(
 
 
 @router.get("/{user_id}/tips/{fitness_goal}")
-def get_workout_tips(user_id, fitness_goal):
+def get_workout_tips(user_id: str, fitness_goal: str):
+    # Internal logic to fetch workout_items from a given user_id
     workout_items = [
         utils.WorkoutItem(
             "1", 4, [8, 7, 5, 4], [95, 145, 165, 170], [60, 120, 180, 240], 185
@@ -24,12 +26,6 @@ def get_workout_tips(user_id, fitness_goal):
         ),
     ]
 
-    return get_workout_tips(workout_items, fitness_goal)
-
-
-def get_workout_tips(
-    workout_items: List[utils.WorkoutItem], fitness_goal: utils.FitnessGoal
-):
     response = []
     for workout_item in workout_items:
         response.append(
@@ -52,3 +48,14 @@ def get_workout_tips(
             }
         )
     return response
+
+@router.get("/{user_id}/distribution/")
+def get_workout_distribution(user_id: str):
+    # Internal logic to associate user_id with workout_item -> workout -> muscle_group
+    workouts = ["forearms", "middle back", "lower back"]
+    workouts = [workout.lower() for workout in workouts]
+    distrib = Counter(workouts)
+    workout_sum = sum(distrib.values())
+    for workout_name, count in distrib.items():
+        distrib[workout_name] = round(count / workout_sum, 2)
+    return distrib
